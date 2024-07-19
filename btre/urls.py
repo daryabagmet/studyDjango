d5919c9ef64cff0c7aaa24f56f400bf181e31428
darya <darya@MBP-darya.Home>
2024-07-19 13:34:35 +0100

    Urls, templates

--- btre/urls.py
+++ btre/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from pages import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('listings/', include('listings.urls')),
     path('admin/', admin.site.urls),
 ]
