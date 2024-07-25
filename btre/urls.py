5f8beedfad244c076e5ec0c773b9d1e7d911e82d
darya <darya@MBP-darya.Home>
2024-07-25 16:15:55 +0100

    Register, login, logout, dashboard pages

--- btre/urls.py
+++ btre/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
     path('listings/', include('listings.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
